b9734b7ebeec1646d462b5b94d71c1651f60f2e1
HaneefSyed29 <shaneef2905@gmail.com>
2024-06-03 10:36:59 +0530

    small updates

--- src/components/Home/homepage.jsx
+++ src/components/Home/homepage.jsx
@@ -23,6 +23,7 @@ const Homepage = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+    console.log(data);
     setMovies(data.Search);
   };
 
